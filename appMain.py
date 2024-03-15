275cacb35b21865c8e71ddeafacd3739bf446e99
Rogério Sobral Ribeiro <sobralcomix@gmail.com>
2024-03-15 10:02:19 -0300

    Rogerio V.1 App escola
    Iniciando a criação do painel

--- appMain.py
+++ appMain.py
@@ -1,59 +1,24 @@
 from flet import *
 from views.viewLogin import ViewLogin
 from views.viewHome import ViewHome
+from views.elements.optionsMenuHide import OptionsMenuHide
 def main(page:Page):
 
 
 
-    optionsMenu=NavigationDrawer(
-        controls=[
-            NavigationDrawerDestination(
-                label="Home",
-                icon=icons.HOME,
-                selected_icon_content=Icon(icons.HOME_OUTLINED),
-            ),
-            Divider(thickness=2),
-            NavigationDrawerDestination(
-                label="Professores",
-                icon=icons.PERSON_ADD_ALT_SHARP,
-                selected_icon_content=Icon(icons.PERSON_ADD_ALT_OUTLINED),
-            ),
-            Divider(thickness=2),
-            NavigationDrawerDestination(
-                label="Matérias",
-                icon=icons.ASSIGNMENT,
-                selected_icon_content=Icon(icons.ASSIGNMENT),
-            ),
-            Divider(thickness=2),
-            NavigationDrawerDestination(
-                label="Aulas",
-                icon=icons.CALENDAR_TODAY,
-                selected_icon_content=Icon(icons.CALENDAR_TODAY_OUTLINED),
-            ),
-            Divider(thickness=2),
-            NavigationDrawerDestination(
-                label="Relatório",
-                icon=icons.FOLDER,
-                selected_icon_content=Icon(icons.FOLDER_OUTLINED),
-            ),
-            Divider(thickness=2),
-            NavigationDrawerDestination(
-                label="Sair",
-                icon=icons.EXIT_TO_APP,
-                selected_icon_content=Icon(icons.EXIT_TO_APP),
-            ),
-
-        ]
-    )
 
     def openMenuHide(e):
         optionsMenu.open=True
         page.update()
 
+    # Views Login
+    telaLogin = ViewLogin()
+    # View home
+    barHome = ViewHome()
+    barHome.btn_menu_hide.on_click = openMenuHide
+    optionsMenu = OptionsMenuHide()
 
-    telaLogin=ViewLogin()
-    barHome=ViewHome()
-    barHome.btn_menu_hide.on_click= openMenuHide
+
 
     telaLogin.btn_enter.on_click=lambda e: page.go("/home")
 
