074584207908f17840212358a4023ca07555d88b
Rogério Sobral Ribeiro <sobralcomix@gmail.com>
2024-03-15 11:01:07 -0300

    Rogerio V.1 Painel Professor
    Tentado centralizar os professores

--- views/painels/painelProfessor.py
+++ views/painels/painelProfessor.py
@@ -2,6 +2,7 @@ from flet import *
 
 class PainelProfessor(UserControl):
     def build(self):
+        self.titulo=Text("Professor",size=38)
         self.t_field_name = TextField(label="Nome")
         self.t_field_sobrenome = TextField(label="Sobrenome",)
         self.drop_contrato=Dropdown(
@@ -11,29 +12,33 @@ class PainelProfessor(UserControl):
                 dropdown.Option("CLT"),
                 dropdown.Option("EVENTUAL"),
                 dropdown.Option("ESTAGIO"),
-            ],
-            autofocus=True,
+            ]
+
         )
 
-        self.list_prof = Column()
+        self.list_prof = Column(alignment=MainAxisAlignment.CENTER)
 
         # application's root control (i.e. "view") containing all other controls
         return Column(
-            width=600,
+
             controls=[
+                Row(controls=[self.titulo],alignment=MainAxisAlignment.CENTER),
+                Divider(thickness=2),
                 Row(
                     controls=[
                         Row(controls=[self.t_field_name,self.t_field_sobrenome,self.drop_contrato]),
                         FloatingActionButton(icon=icons.ADD, on_click=self.add_clicked),
                     ],
+                    alignment=MainAxisAlignment.CENTER
                 ),
                 self.list_prof,
             ],
+
         )
 
     def add_clicked(self, e):
         self.list_prof.controls.append(Checkbox(label=f"{self.t_field_name.value} | {self.t_field_sobrenome.value} | "
-                                                      f"{self.drop_contrato}"))
+                                                      f"{self.drop_contrato.value}"))
         self.t_field_name.value= ""
         self.t_field_sobrenome.value=""
         self.update()
