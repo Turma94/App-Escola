cf1bbc34acd2dcdf064dbbb650c9ad57c8e42f17
Rogério Sobral Ribeiro <sobralcomix@gmail.com>
2024-03-21 11:20:58 -0300

    Rogério Atualização na criptografia
    Testando a criptografia da senha

--- utils/criptografia.py
+++ utils/criptografia.py
@@ -1,14 +1,21 @@
 from hashlib import sha256
+from hashlib import sha256
 
-senha="1234"
-hash_senha=sha256(senha.encode())
-print(hash_senha.hexdigest())
+def criptografarSenha(senha):
+    senha = sha256(senha.encode())
+    return senha.hexdigest()
+
+if __name__ == '__main__':
+
+    senha="1234"
+    hash_senha=sha256(senha.encode())
+    print(hash_senha.hexdigest())
 
 
-senhaDigitada=input("digite sua senha: ")
-hash_senha_digitada=sha256(senhaDigitada.encode())
+    senhaDigitada=input("digite sua senha: ")
+    hash_senha_digitada=sha256(senhaDigitada.encode())
 
-if hash_senha.digest()==hash_senha_digitada.digest():
-    print("Validado")
-else:
-    print("Validado")
+    if hash_senha.digest()==hash_senha_digitada.digest():
+        print("Validado")
+    else:
+        print("Validado")
