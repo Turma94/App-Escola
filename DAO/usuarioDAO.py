8d5f8123af90c95d8b41915cb6975943c5937199
Rogério Sobral Ribeiro <sobralcomix@gmail.com>
2024-04-01 11:53:02 -0300

    Update usuarioDAO.py
    aTUALIZANDO

--- DAO/usuarioDAO.py
+++ DAO/usuarioDAO.py
@@ -53,9 +53,9 @@ if __name__ == '__main__':
 
     # senhaGerada=criptografarSenha(input("digite uma senha: "))
     #
-    addUsuario("maria","rocha",
-               "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4",
-               "maria@gmail.com","ADM")
+    # addUsuario("maria","rocha",
+    #            "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4",
+    #            "maria@gmail.com","ADM")
 
     for i in listarUsuario():
         print(i)
